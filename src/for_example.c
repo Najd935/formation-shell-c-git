83c9d0c419b01888e520cad06c667611b44075f8
Najd Kacem <najd.code@gmail.com>
2025-04-28 03:07:56 +0200

    modification de for_example pour n'afficher que les nombres pairs

--- src/for_example.c
+++ src/for_example.c
@@ -5,7 +5,11 @@ int main()
 {
     for (int i = 2; i <= 20; i++)
     {
-        printf("%d\n",i);
+        if (i%2 == 0) 
+        {
+            printf("%d\n",i);
+        }
+       
     }
     return 0;
 }
