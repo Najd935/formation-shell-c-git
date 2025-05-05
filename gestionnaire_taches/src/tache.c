a31e02dbddb56c9423fef6c82c0ef3b8c22598cb
Najd Kacem <najd.code@gmail.com>
2025-05-05 14:21:37 +0200

    restructuration de larboresence et début d'un nouveau projet de gestionnaire de tâches.

---
+++ gestionnaire_taches/src/tache.c
@@ -0,0 +1,9 @@
+#include "tache.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+
+
+void ajouter_tache(Tache** taches, int* nb_taches, int* capacite);
+void afficher_taches(Tache* taches, int nb_taches);
